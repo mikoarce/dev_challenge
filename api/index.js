e10139a32eb160cdb01bb5cf07cd34a6e70bffe5
marce <mikoarce@gmail.com>
2019-12-22 12:18:02 -0500

    installed axios and allowed cors for dev env.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const apiRoutes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({
+    origin: 'http://localhost:8080',
+  }));
+}
 
 app.use('/api', apiRoutes);
 app.listen(port, () => console.log(`Running on port ${port}`));
