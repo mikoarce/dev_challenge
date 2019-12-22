0904b2f6146663cdf81873b1126b773fd6c729a6
marce <mikoarce@gmail.com>
2019-12-22 09:07:47 -0500

    Initialize React frontend with webpack and docker boilerplate.

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'styles/main.css';
+import MainView from 'views/MainView';
+
+render(<MainView />, document.getElementById('app'));
---
+++ client/views/MainView.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MainView = () => (<div>HELLO WORLD</div>);
+
+export default MainView;
