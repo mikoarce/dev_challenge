6316c9cb00003909f8ea410f2106908ff03df9e4
marce <mikoarce@gmail.com>
2019-12-22 11:41:50 -0500

    Transform max field into controlled component and added form validator.

--- client/forms/GetPrimeMedianForm.jsx
+++ client/forms/GetPrimeMedianForm.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
 
+const numberValidator = (input) => {
+  const defaultError = { hasError: true, message: 'Input must be a whole number' };
+  const noError = { hasError: false };
+
+  if (input === '') {
+    return noError;
+  }
+
+  if (Number.isNaN(Number(input))) {
+    return defaultError;
+  }
+
+  const currVal = Number(input);
+  return currVal % 1 === 0 ? noError : defaultError;
+};
+
 const GetPrimeMedianForm = () => {
   const [max, setMax] = React.useState('');
-  const onMaxChange = React.useCallback((_, { value }) => setMax(value), []);
+  const [maxFieldError, setMaxFieldError] = React.useState(undefined);
+
+  const onMaxChange = React.useCallback((_, { value }) => {
+    const { hasError, message } = numberValidator(value);
+    if (hasError) {
+      setMaxFieldError({ content: message });
+    } else {
+      setMaxFieldError(undefined);
+    }
+    setMax(value);
+  }, []);
+
   return (
     <Form>
       <Form.Input
@@ -11,8 +38,12 @@ const GetPrimeMedianForm = () => {
         name="max"
         value={max}
         onChange={onMaxChange}
+        error={maxFieldError}
       />
-      <Form.Button content="Submit" />
+      <Form.Button
+        content="Submit"
+        disabled={max.length === 0 || !!maxFieldError}
+      />
     </Form>
   );
 };
